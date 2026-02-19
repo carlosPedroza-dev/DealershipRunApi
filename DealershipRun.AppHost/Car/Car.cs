7efd94de00dbc30b1a0a38d3ffa7ec1a37b5afe0
carlosPedroza-dev <carlospedroza.dev@gmail.com>
2026-02-18 17:28:27 -0700

    Add Car model

---
+++ DealershipRun.AppHost/Car/Car.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DealershipRun.AppHost.Car
+{
+    public class Car
+    {
+        private long id { get; set; }
+        private string brand { get; set; }
+        private string model { get; set; }
+        private int year { get; set; }
+        private double price { get; set; }
+        private double mileage { get; set; }
+        private CarStatus carStatus { get; set; }
+
+    }
+}
---
+++ DealershipRun.AppHost/Car/CarStatus.cs
@@ -0,0 +1,6 @@
+﻿public enum CarStatus
+{
+    Available,
+    Reserved,
+    Sold
+}
