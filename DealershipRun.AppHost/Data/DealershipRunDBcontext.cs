ae0a7d57a98e28026780456a1c020fa480004348
carlosPedroza-dev <carlospedroza.dev@gmail.com>
2026-03-20 11:05:38 -0600

    add DBcontext file with DbSet of Car,Order and User Entities

---
+++ DealershipRun.AppHost/Data/DealershipRunDBcontext.cs
@@ -0,0 +1,19 @@
+﻿using DealershipRun.AppHost.Car;
+using DealershipRun.AppHost.Order;
+using DealershipRun.AppHost.User;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace DealershipRun.AppHost.Data
+{
+    public class DealershipRunDBcontext : DbContext
+    {
+        public DealershipRunDBcontext(DbContextOptions<DealershipRunDBcontext> options)  
+        :base(options){ }
+
+        public DbSet<CarEntity> Cars { get; set; }
+        public DbSet<OrderEntity> Orders { get; set; }
+        public DbSet<UserEntity> Users { get; set; }
+
+        }
+    }
