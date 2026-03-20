2c2a3d6e2a9644bc07ff16ef84907b33694e7273
carlosPedroza-dev <carlospedroza.dev@gmail.com>
2026-03-20 10:57:48 -0600

    add column and table annotations to parameters that doesnt have one

--- DealershipRun.AppHost/Car/CarEntity.cs
+++ DealershipRun.AppHost/Car/CarEntity.cs
@@ -2,27 +2,36 @@
 
 
 using DealershipRun.AppHost.Order;
+using k8s.Models;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DealershipRun.AppHost.Car
 {
+    [Table("cars")]
     public class CarEntity
     {
         [Key]
         public long Id { get; set; }
         [Required]
         [MaxLength(20)]
-        public string Brand { get; set; }
+        [Column("brand")]
+        public string Brand { get; set; } = string.Empty;
         [Required]
         [MaxLength(20)]
-        public string Model { get; set; }
+        [Column("model")]
+        public string Model { get; set; } = string.Empty;
         [Required]
+        [Column("year")]
         public int Year { get; set; }
         [Required]
+        [Column("price")]
         public decimal Price { get; set; }
         [Required]
+        [Column("mileage")]
         public decimal Mileage { get; set; }
         [Required]
+        [Column("car_status")]
         public CarStatus CarStatus { get; set; }
 
         public List<OrderEntity> Orders { get; set; } = [];
