007e1b37aec39be49c699fcbffafc0412dd12509
carlosPedroza-dev <carlospedroza.dev@gmail.com>
2026-03-20 10:32:29 -0600

    rearrenged,rename and add new parameters to all entities. And finish all base DTOs

--- DealershipRun.AppHost/User/UserEntity.cs
+++ DealershipRun.AppHost/User/UserEntity.cs
@@ -8,26 +8,29 @@ using System.Text.Json.Serialization;
 namespace DealershipRun.AppHost.User
 {
     [Table("users")]
-    public class User
+    public class UserEntity
     {
         [Key]
         public long Id { get; set; }
         [Required]
         [MaxLength(30)]
         [Column("user_name")]
-        public string UserName { get; set; }
+        public string UserName { get; set; } = string.Empty;
         [Column("email")]
         [Required]
         [EmailAddress]
         [MaxLength(50)]
-        
-        public string Email { get; set; }
+
+        public string Email { get; set; } = string.Empty;
         [Required]
         [JsonIgnore]
-        [MaxLength(20)]
-        public string PasswordHash { get; set; }
+        [MaxLength(60)]
+        public string PasswordHash { get; set; } = string.Empty;
+
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
         public Role Role { get; set; }
-        public List<OrderEntity> Orders{get;set;}
+        public List<OrderEntity> Orders { get; set; } = [];
 
     }
 }
