a0d74a3ed706aa587da069e1e12b441891cbaf21
carlosPedroza-dev <carlospedroza.dev@gmail.com>
2026-03-21 21:04:10 -0600

    make IUserService

---
+++ DealershipRun.AppHost/User/IUserService.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace DealershipRun.AppHost.User
+{
+    public interface IUserService
+    {
+        Task register(UserEntity user);
+        Task Login(string username, string password);
+        Task<UserEntity> getById(long id);
+        Task delete(long id);
+        Task updateRole(long id, Role role);
+    }
+}
