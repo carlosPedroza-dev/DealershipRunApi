a275a2af837326b4e7d6d0a278d9911a5e03572a
carlosPedroza-dev <carlospedroza.dev@gmail.com>
2026-03-21 21:00:36 -0600

    add IOrderService and modify string status function parameter in ICarService. Now It is tied to the enum CarStatus

--- DealershipRun.AppHost/Car/ICarService.cs
+++ DealershipRun.AppHost/Car/ICarService.cs
@@ -9,7 +9,7 @@ namespace DealershipRun.AppHost.Car
         Task<CarEntity> createCar(CarEntity car);
         Task Update(long id, CarEntity car);
         Task Delete(long id);
-        Task UpdateStatus(long id, string status);
+        Task UpdateStatus(long id, CarStatus status);
 
     }
 }
---
+++ DealershipRun.AppHost/Order/IOrderService.cs
@@ -0,0 +1,15 @@
+﻿
+
+using Microsoft.EntityFrameworkCore.Update.Internal;
+
+namespace DealershipRun.AppHost.Order
+{
+    public interface IOrderService
+    {
+        Task createOrder(long userId, long carId);
+        Task getOrdersByUser(long userId);
+        Task<OrderEntity> GetById(long id);
+        Task updateStatus(long id, OrderStatus status);
+        Task cancelOrder(long orderId);
+    }
+}
