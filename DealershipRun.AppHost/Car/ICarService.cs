faa0d8475b2b3b436185abc1f653d7838e262fdc
carlosPedroza-dev <carlospedroza.dev@gmail.com>
2026-03-21 20:54:23 -0600

    add ICarService which is a interface for CarService functions

---
+++ DealershipRun.AppHost/Car/ICarService.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace DealershipRun.AppHost.Car
+{
+    public interface ICarService
+    {
+        Task<List<CarEntity>> getAll();
+        Task<CarEntity?> getById(long id);
+        Task<CarEntity> createCar(CarEntity car);
+        Task Update(long id, CarEntity car);
+        Task Delete(long id);
+        Task UpdateStatus(long id, string status);
+
+    }
+}
