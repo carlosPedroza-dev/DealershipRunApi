651d61bf5a2d0af1c9a77ac35f54ff7336786ce2
carlosPedroza-dev <carlospedroza.dev@gmail.com>
2026-03-23 20:09:49 -0600

    configure CarRepository with interface ICarRepository

---
+++ DealershipRun.AppHost/Car/ICarRepository.cs
@@ -0,0 +1,13 @@
+﻿
+namespace DealershipRun.AppHost.Car
+{
+    public interface ICarRepository
+    {
+        Task<List<CarEntity>> getAll();
+        Task<CarEntity?> getById(long id);
+        Task<CarEntity> createCar(CarEntity car);
+        Task Update(CarEntity car);
+        Task Delete(long id);
+        Task UpdateStatus(long id, CarStatus status);
+    }
+}
