73d6a088ed8bb6b44d378842e397938faeabb0af
carlosPedroza-dev <carlospedroza.dev@gmail.com>
2026-03-23 22:43:09 -0600

    finish CarController,instead of using CarEntity. CarController was modified in order to use CarDTO, avoiding exposiong database data

--- DealershipRun.AppHost/Car/CarController.cs
+++ DealershipRun.AppHost/Car/CarController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace DealershipRun.AppHost.Car
 {
     [ApiController]
+    [Route("DealershipRun/Cars")]
     public class CarController : ControllerBase
     {
         private readonly ICarService _carService;
@@ -17,7 +18,10 @@ namespace DealershipRun.AppHost.Car
         public async Task<IActionResult> getAll()
         {
             var cars = await _carService.getAll();
-            return Ok(cars);
+            var result = cars.Select(c => new CarDTO(
+                c.Id, c.Brand, c.Model, c.Year, c.Price, c.Mileage, c.CarStatus
+            ));
+            return Ok(result);
         }
 
         [HttpGet("{id}")]
@@ -26,14 +30,33 @@ namespace DealershipRun.AppHost.Car
             return Ok(car);
         }
         [HttpPost]
-        public async Task<IActionResult> Create([FromBody] CarEntity car)
+        public async Task<IActionResult> Create([FromBody] CarDTO dto)
         {
+            var car = new CarEntity
+            {
+                Brand = dto.Brand,
+                Model = dto.Model,
+                Year = dto.Year,
+                Price = dto.Price,
+                Mileage = dto.Milleage,
+                CarStatus = CarStatus.Available
+            };
             var created = await _carService.createCar(car);
-            return CreatedAtAction(nameof(getById), new { id = created.Id }, created);
+            return CreatedAtAction(nameof(getById), new { id = created.Id }, new CarDTO(
+                created.Id, created.Brand, created.Model, created.Year, created.Price, created.Mileage, created.CarStatus
+            ));
         }
         [HttpPut("{id}")]
-        public async Task<IActionResult> Update(long id, [FromBody] CarEntity car)
+        public async Task<IActionResult> Update(long id, [FromBody] CarDTO dto)
         {
+            var car = new CarEntity
+            {
+                Brand = dto.Brand,
+                Model = dto.Model,
+                Year = dto.Year,
+                Price = dto.Price,
+                Mileage = dto.Milleage
+            };
             await _carService.Update(id, car);
             return NoContent();
         }
